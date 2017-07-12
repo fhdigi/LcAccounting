fa3d89550c1aa542d46523121f7049680ef73a75
Freddie Heidrich <fredheidrich@lccsny.com>
2017-07-12 14:32:04 -0400

    Added Jsons

--- LcAccountingApplication/Models/Employee.cs
+++ LcAccountingApplication/Models/Employee.cs
@@ -4,34 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.WindowsAzure.MobileServices;
+using Newtonsoft.Json;
 
 namespace LcAccountingApplication.Models
 {
     public class Employee
     {
-        public string LastName { get; set; }
-        public string FirstName { get; set; }
-        public string MiddleInitial { get; set; }
-        public int PinNumber { get; set; }
-        public bool IsAdmin { get; set; }
-        public string Address { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public string ZipCode { get; set; }
-        public string PhoneNumber { get; set; }
-        public string SocialSecurityNumber { get; set; }
-        public DateTime DateHired { get; set; }
-        public string PayType { get; set; }
-        public double PayRate { get; set; }
-        public double AdditionalPay { get; set; }
-        public string EmergencyContact { get; set; }
-        public string EmergencyRelationship { get; set; }
-        public string EmergencyPhone { get; set; }
-        public string MedicalCondition { get; set; }
-        public string Notes { get; set; }
-        public bool Inactive { get; set; }
-        public DateTime DateOfBirth { get; set; }
-        public DateTime DateTermination { get; set; }
+        [JsonProperty(PropertyName = "LastName")] public string LastName { get; set; }
+        [JsonProperty(PropertyName = "FirstName")] public string FirstName { get; set; }
+        [JsonProperty(PropertyName = "MiddleInitial")] public string MiddleInitial { get; set; }
+        [JsonProperty(PropertyName = "PinNumber")] public int PinNumber { get; set; }
+        [JsonProperty(PropertyName = "IsAdmin")] public bool IsAdmin { get; set; }
+        [JsonProperty(PropertyName = "Address")] public string Address { get; set; }
+        [JsonProperty(PropertyName = "City")] public string City { get; set; }
+        [JsonProperty(PropertyName = "State")] public string State { get; set; }
+        [JsonProperty(PropertyName = "ZipCode")] public string ZipCode { get; set; }
+        [JsonProperty(PropertyName = "PhoneNumber")] public string PhoneNumber { get; set; }
+        [JsonProperty(PropertyName = "SocialSecurityNumber")] public string SocialSecurityNumber { get; set; }
+        [JsonProperty(PropertyName = "DateHired")] public DateTime DateHired { get; set; }
+        [JsonProperty(PropertyName = "PayType")] public string PayType { get; set; }
+        [JsonProperty(PropertyName = "PayRate")] public double PayRate { get; set; }
+        [JsonProperty(PropertyName = "AdditionalPay")] public double AdditionalPay { get; set; }
+        [JsonProperty(PropertyName = "EmergeencyContact")] public string EmergencyContact { get; set; }
+        [JsonProperty(PropertyName = "EmergencyRelationship")] public string EmergencyRelationship { get; set; }
+        [JsonProperty(PropertyName = "EmergencyPhone")] public string EmergencyPhone { get; set; }
+        [JsonProperty(PropertyName = "MedicalCondition")] public string MedicalCondition { get; set; }
+        [JsonProperty(PropertyName = "Notes")] public string Notes { get; set; }
+        [JsonProperty(PropertyName = "Inactive")] public bool Inactive { get; set; }
+        [JsonProperty(PropertyName = "DateOfBirth")] public DateTime DateOfBirth { get; set; }
+        [JsonProperty(PropertyName = "DateTermination")] public DateTime DateTermination { get; set; }
 
         private static readonly IMobileServiceTable<Employee> BillsTable = App.MobileService.GetTable<Employee>();
 
