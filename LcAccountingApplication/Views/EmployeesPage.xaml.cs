3aec1cae4865b037f83ca425a1dd33ffc7747992
Fred Heidrich <heidrich@lccsny.com>
2017-07-12 12:41:40 -0400

    Tweaked the employee page

--- LcAccountingApplication/Views/EmployeesPage.xaml.cs
+++ LcAccountingApplication/Views/EmployeesPage.xaml.cs
@@ -11,40 +11,5 @@ namespace LcAccountingApplication.Views
         {
             InitializeComponent();
         }
-
-        private void textBlock_SelectionChanged(object sender, Windows.UI.Xaml.RoutedEventArgs e)
-        {
-
-        }
-
-        private void textBlock2_SelectionChanged(object sender, Windows.UI.Xaml.RoutedEventArgs e)
-        {
-
-        }
-
-        private void textBlock4_SelectionChanged(object sender, Windows.UI.Xaml.RoutedEventArgs e)
-        {
-
-        }
-
-        private void textBlock7_SelectionChanged(object sender, Windows.UI.Xaml.RoutedEventArgs e)
-        {
-
-        }
-
-        private void textBlock9_SelectionChanged(object sender, Windows.UI.Xaml.RoutedEventArgs e)
-        {
-
-        }
-
-        private void listBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-
-        private void textBlock10_SelectionChanged(object sender, Windows.UI.Xaml.RoutedEventArgs e)
-        {
-
-        }
     }
 }
